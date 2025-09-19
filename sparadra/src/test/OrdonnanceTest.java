3257c6d9f3615076f9ab9dfc71a1a12bf60b636a
goodspeed57 <juju.taesch@gmail.com>
2025-09-19 02:18:23 +0200

    Test unitaire

---
+++ sparadra/src/test/OrdonnanceTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdonnanceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
